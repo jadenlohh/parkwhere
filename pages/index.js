497dfc002f0479cbf50ab7e03c6d2dd09d6582d9
Jaden Loh <jadenlohh@gmail.com>
2023-08-19 13:39:58 +0800

    initial create

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Layout from '@/components/layout'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>carpark locator</title>
+      </Head>
+
+      <h2>This is the home page</h2>
+    </Layout>
+  )
+}
