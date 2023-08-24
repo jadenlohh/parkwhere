34597b6448427ce25b154300e51d5cf5d503b449
Jaden Loh <jadenlohh@gmail.com>
2023-08-24 12:48:29 +0800

    feat: fetch data from url and display in card

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,106 @@
 import styles from '../styles/Home.module.css'
 import Layout from '../components/layout'
 import Head from 'next/head'
+import useSWR from 'swr'
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Home() {
-  return (
-    <Layout>
-      <Head>
-        <title>carpark locator</title>
+	const { data, error, isLoading } = useSWR('https://api.data.gov.sg/v1/transport/carpark-availability', fetcher)
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
+	if (error) return "An error has occurred."
 
-        <script src="https://kit.fontawesome.com/aa241108d7.js" crossorigin="anonymous"></script>
-      </Head>
+	if (isLoading) return (
+		<div className='container'>
+			<div className={styles.searchBarContainer}>
+				<form>
+					<input type='text' className={styles.searchBar} placeholder='Search by address' />
+				</form>
+			</div>
 
-      <div className='container'>
-        <div className={styles.searchBarContainer}>
-          <form>
-            <input type='text' className={styles.searchBar} placeholder='Search by address' />
-          </form>
-        </div>
+			<div className='row gy-4'>
+				{
+					[...Array(20).keys()].map(i => {
+						return (
+							<div className='col-md-4 col-sm-12' key={i}>
+								<div className={`${styles.card} ${styles.skeleton}`}>
+									<h5></h5>
 
-        <div className='row'>
-          <div className='col-md-4 col-sm-12'>
-            <div className={styles.card}>
-              <h5>BM29 <span className={styles.distance}>(123m away)</span></h5>
+									<div className={styles.cardBody}>
+										<p></p>
 
-              <div className={styles.cardBody}>
-                <p className={styles.location}><i className="fa-sharp fa-solid fa-location-dot"></i> <span>BLK 207/208 JURONG EAST STREET 21</span></p>
+										<p></p>
 
-                <p className={styles.lotsAvailable}><i className="fa-solid fa-square-parking"></i> <span>20 Lots Available</span></p>
+										<div className={styles.tagsContainer}>
+											<span className={styles.tags}></span>
+											<span className={styles.tags}></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						)
+					})
+				}
+			</div>
+		</div>
+	)
 
-                <div className={styles.tagsContainer}>
-                  <span className={`${styles.tags} ${styles.availabilityTag}`}>Available</span>
-                  <span className={`${styles.tags} ${styles.freeParkingTag}`}>Free Parking</span>
-                </div>
+	return (
+		<Layout>
+			<Head>
+				<title>Carpark Locator</title>
 
-                <div className={styles.buttonContainer}>
-                  <div className='row align-items-center'>
-                    <div className='col-6'>
-                      <button type='button' className={styles.btn}>Details</button>
-                    </div>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
 
-                    <div className='col-6'>
-                      <button type='button-outline' className={`${styles.btn} ${styles.btnOutline}`}>View on Map</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
+				<script src="https://kit.fontawesome.com/aa241108d7.js" crossorigin="anonymous"></script>
+			</Head>
+
+			<div className='container'>
+				<div className={styles.searchBarContainer}>
+					<form>
+						<input type='text' className={styles.searchBar} placeholder='Search by address' />
+					</form>
+				</div>
+
+				<div className='row gy-4'>
+					{
+						data.items[0].carpark_data.map((item, index) => {
+							return (
+								<div className='col-md-4 col-sm-12' key={index}>
+									<div className={styles.card}>
+										<h5>{item.carpark_number} <span className={styles.distance}>(123m away)</span></h5>
+
+										<div className={styles.cardBody}>
+											<p className={styles.location}><i className="fa-sharp fa-solid fa-location-dot"></i> <span>BLK 207/208 JURONG EAST STREET 21</span></p>
+
+											<p className={styles.lotsAvailable}><i className="fa-solid fa-square-parking"></i> <span>{item.carpark_info[0].lots_available} Lots Available</span></p>
+
+											<div className={styles.tagsContainer}>
+												<span className={`${styles.tags} ${styles.availabilityTag}`}>Available</span>
+												<span className={`${styles.tags} ${styles.freeParkingTag}`}>Free Parking</span>
+											</div>
+
+											<div className={styles.buttonContainer}>
+												<div className='row align-items-center'>
+													<div className='col-6'>
+														<button type='button' className={styles.btn}>Details</button>
+													</div>
+
+													<div className='col-6'>
+														<button type='button-outline' className={`${styles.btn} ${styles.btnOutline}`}>View on Map</button>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							)
+						})
+					}
+				</div>
+			</div>
+		</Layout>
+	)
 }
