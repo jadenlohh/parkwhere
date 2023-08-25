5f47a7100c478b87deb07ca9e589b743e006eba1
Jaden Loh <jadenlohh@gmail.com>
2023-08-25 16:40:51 +0800

    chore: move Card style to new file

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Card.module.css'
 
 const ConditionalWrapper = ({vehicleType}) => {
   if (vehicleType == 'C') {
