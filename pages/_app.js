db1904e704bc3f90f1145526851d0a64e8134b3b
Jaden Loh <jadenlohh@gmail.com>
2023-08-25 17:44:36 +0800

    add bootstrap css

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
+import Script from 'next/script'
 import '../styles/global.css'
 import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
-
-				<script src="https://kit.fontawesome.com/aa241108d7.js" crossorigin="anonymous"></script>
+				<Script src="https://kit.fontawesome.com/aa241108d7.js" crossorigin="anonymous" />
 			</Head>
 
 			<Component {...pageProps} />
