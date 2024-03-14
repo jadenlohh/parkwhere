22ef804404c4082f4deed1fd144d8d72aa4bc9d7
jadenlohh <jadenlohh@gmail.com>
2024-03-14 21:06:14 +0800

    chore: remove loading function

--- src/component/carpark/CarparkList.jsx
+++ src/component/carpark/CarparkList.jsx
@@ -5,26 +5,17 @@ import Carpark from './Carpark';
 
 const CarparkList = () => {
   const [carparks, setCarparks] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-    async function startFetching() {
-      await axios.get('https://parkwhere-api.vercel.app').then(response => {
-        if (!ignore) {
-          setCarparks(response.data)
-        }
-      })
-    }
-
     let ignore = false
-    // setIsLoading(true)
 
-    startFetching()
-    // setIsLoading(false)
+    axios.get('https://parkwhere-api.vercel.app').then(response => {
+      if (!ignore) {
+        setCarparks(response.data)
+      }
+    })
 
-    return () => { 
-      ignore = true 
-    }
+    return () => { ignore = true }
   }, [])
 
   return (
@@ -34,18 +25,15 @@ const CarparkList = () => {
       </div>
 
       <div className='container'>
-        <ul className='row mt-4 ps-0 carpark-list'>
+        <ul className='row mt-2 ps-0 carpark-list'>
           {
-            isLoading ? 
-              <p>Loading...</p> : 
-              
-              carparks.map((item, index) => {
-                return (
-                  <li key={index} className='col-md-4 col-sm-12 mb-4'>
-                    <Carpark name={item.name} availableLots={item.availableLots} code={item.carparkID} lotType={item.lotType} agency={item.agency} />
-                  </li>
-                )
-              })
+            carparks.map((item, index) => {
+              return (
+                <li key={index} className='col-md-4 col-sm-12 mb-4'>
+                  <Carpark name={item.name} availableLots={item.availableLots} code={item.carparkID} lotType={item.lotType} agency={item.agency} />
+                </li>
+              )
+            })
           }
         </ul>
       </div>
