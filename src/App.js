c0c20d1c65f30aec098e7449042749303f022b8f
jadenlohh <jadenlohh@gmail.com>
2024-04-11 17:01:13 +0800

    chore: remove unused stylesheet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import CarparkDetails from './pages/CarparkDetails';
