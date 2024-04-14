6987762c6d7a1e4b4c2a022cad3f06618307f9c2
jadenlohh <jadenlohh@gmail.com>
2024-04-14 18:03:44 +0800

    update grammar for lots available

--- src/component/carpark/Carpark.jsx
+++ src/component/carpark/Carpark.jsx
@@ -48,7 +48,7 @@ const Carpark = ({ name, availableLots, carparkID, lotType, agency }) => {
       <div className='carpark-info-card text-start'>
         <div className='d-flex flex-row justify-content-between'>
           <div>
-            <p className='lots-available'>{availableLots} Lots Available</p>
+            {availableLots <= 1 ? <p className='lots-available'>{availableLots} Lot Available</p> : <p className='lots-available'>{availableLots} Lots Available</p>}
           </div>
 
           <div className='align-self-center text-center carpark-icon-container'>
